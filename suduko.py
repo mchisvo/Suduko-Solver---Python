54bf637d9b54fd88c876fd5d11d135666f11ca1f
Michael Lampard <mlampa01@dcs.bbk.ac.uk>
2018-02-18 18:46:00 +0000

    Finished the program

--- suduko.py
+++ suduko.py
@@ -1,5 +1,6 @@
-import time
-
+"""Michael Lampard Chisvo mlampa01 ID No. 13132791
+    This program is a Suduko solver. The input is a 2d array of integers that represent a Suduko board. The
+     program will then attempt to solve the puzzle and print out the result including unsolved locations."""
 
 def read_sudoku(file):
     stream = open(file)
@@ -9,19 +10,20 @@ def read_sudoku(file):
 
 
 def print_sudoku(problem):
-    """ Prints out a suduko grid representing the problem to be solved"""
+    """ Prints out a Suduko grid representing the problem to be solved"""
     print("-"*37)
     for row, cols in enumerate(problem,1): # Enumerate each list beggining with one
         print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else "." for x in cols])) #String format and list comprehension based on the list
         if row == 9:  # On the 9th list print a seperating line.
             print("-"*37)
         elif row % 3 == 0:  # After ever 3rd list print a seperating line
-            print("|" + "-"*35 + "|")
+            print("|" + "---+"*8 + "---|")
+
 
 def convertToSets(problem):
     """Creates and returns a new 2d array of sets. For each location  with a number 1 to 9, a singleton set
-        of that number is created. For each ,location with a zero, create a set containing numbers 1 through 9.
-        i is row and j is column. consider re-writting with i and j BLLALALALALALA
+        of that number is created. For each ,location with a zero, creates a set containing numbers 1 through 9.
+        i is row and j is column.
         """
     s = set(range(1, 10))
     for i, row in enumerate(problem):  # Gives a row index and the row
@@ -33,7 +35,20 @@ def convertToSets(problem):
     return problem
 
 
+def convertToInts_failed(problem):
+    "If the solving has failed, the unsolved locations are given a value of 0"
+    for i, row in enumerate(problem):  # Gives a row index and the row
+        for j, num in enumerate(row):
+            problem[i][j] = list(num)
+            if len(problem[i][j]) == 1:
+                problem[i][j] = problem[i][j][0]
+            else:
+                problem[i][j] = 0
+    return problem
+
+
 def convertToInts(problem):
+    "Converts a 2d array of sets to integers"
     for i, row in enumerate(problem):  # Gives a row index and the row
         for j, num in enumerate(row):
             problem[i][j] = list(num)
@@ -41,11 +56,11 @@ def convertToInts(problem):
                 problem[i][j] = problem[i][j][0]
     return problem
 
+
 def getRowLocations(rowNumber):
     """Function that returns a list of tuples, each specifying the location of elements of a row based on a row number
     (i,j)"""
     return sorted([(rowNumber, j) for j in range(0, 9)])
-
 
 
 def getColumnLocations(columnNumber):
@@ -55,6 +70,7 @@ def getColumnLocations(columnNumber):
 
 
 def getBoxLocations(location):
+    """Finds box locations by finding the top left coordinates of the box"""
     row_start = location[0] // 3 * 3  # mathematical jiggery pokery
     col_start = location[1] // 3 * 3
     box_locations = []
@@ -84,26 +100,22 @@ def eliminate(problem, location, listOfLocations):
 
 
 def solve(problem):
-    """This function changes the array problem and returns True if the problem has been solved, False otherwise.
+    """Function changes the array problem and returns True if the problem has been solved, False otherwise.
     Here's what this function needs to do. For every location in the array, call eliminate with row, column, and box
     locations. If any values have been eliminated (eliminate returns something other than zero), repeat this procedure.
      When it is no longer possible to eliminate anything, return the boolean result.
     """
-    count = 0
     while not isSolved(problem):
+        count = 0
         for rowi, row in enumerate(problem):
             for colj, num in enumerate(problem):
                 if len(problem[rowi][colj]) == 1: # If a singleton is found eliminate it from row, columns and box
                     location = (rowi, colj)
                     listOfLocations = getBoxLocations(location) + getColumnLocations(location[1]) + getRowLocations(location[0])
                     count += eliminate(problem, location, listOfLocations)
-                    print(count)
-                #if(rowi, colj) == (len(problem), len(problem[0])) and count == 0: # If on the last element stop the program
-
-    print(problem)
-    convertToInts(problem)
-    print(problem)
-    print_sudoku(problem)
+        if count == 0:  # If nothing changed on the last run through, the solver cannot complete the puzzle.
+            return False
+    return True
 
 def isSolved(problem):
     """Given a two-dimensional array problem of sets, return True if the Sudoku problem has been solved
@@ -114,21 +126,42 @@ def isSolved(problem):
                 return False
     return True
 
+
 def main():
-    print("Please provide file name of puzzle", end='')
-    file= input()
-    problem = read_sudoku(file)
-    print_sudoku(problem)
-    problem = convertToSets(problem)
+    """Main function reads in the file dependent on user input and attempts to solve the Suduko. If this is not possible
+    a dictionary of unsolved locations is generate and the puzzle is printed with "." representing unsolved locations.
+    """
+    again = ('y', 'Y')  # Tuple used to decide if user wants to play again
+    while True:
+        print("Please provide file name of puzzle", end='')
+        file = input()
+        problem = read_sudoku(file)
+        print_sudoku(problem)
+        problem = convertToSets(problem)
+        if solve(problem):
+            convertToInts(problem)
+            print_sudoku(problem)
+        else:
+            dict = {}
+            # this wing will produce a dictionary of locations where there is no singleton
+            for rowi, row in enumerate(problem):
+                for colj, num in enumerate(problem):
+                    if len(problem[rowi][colj]) != 1:
+                        dict[(rowi, colj)] = (problem[rowi][colj])
+            print("Locations not solved are printed below", '\n', dict)
+            convertToInts_failed(problem)
+            print("Incomplete puzzle below")
+            print_sudoku(problem)
+        # The below statements allow the user to start again with a new puzzle.
+        print('\n', "Would you like to play again? (y or Y to start again), press any key to exit.", end='')
+        answer = input()
+        if answer in again:  # Statement to check if the user input a valid response to play again
+            print('\n', "Here we go again!", '\n')
 
-    solve(problem)
-    #listOfLocations = []
-    #for rowi, row in enumerate(problem):
-       # for colj, num in enumerate(problem):
-        #    if problem[rowi][colj].issubset
-        #    listOfLocations.append((rowi, colj))
-    # Need a for loop to step through each location and eliminate numbers based on eliminate function.
-   # #print(listOfLocations)
+            continue
+        else:
+            break
 
 
-main()
+if __name__ == "__main__":
+    main()
